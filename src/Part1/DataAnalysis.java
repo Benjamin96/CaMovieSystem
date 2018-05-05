d254aa2bd33858f6c3d71c71559611cc5974b03f
Ben <Ben@DESKTOP-3HFFUDM.mshome.net>
2018-05-05 10:17:22 +0100

    Forgot to move the .csv File

--- src/Part1/DataAnalysis.java
+++ src/Part1/DataAnalysis.java
@@ -19,7 +19,7 @@ public class DataAnalysis {
         HashTableGeneral<Movie> MovieHashTable = new HashTableGeneral();
         ArrayList < Movie > MovieList = new ArrayList();
         
-        String csvFile = "C:\\Users\\Ben\\Desktop\\Duo Ca\\MovieSystem\\samplemovies.csv";
+        String csvFile = "C:\\Users\\Ben\\Documents\\NetBeansProjects\\CaMovieSystemBen&Chris\\src\\Part1\\samplemovies.csv";
         String line = "";
         //Main Hash table for overview of Movies from CSV
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
